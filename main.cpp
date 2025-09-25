d88ac8625a3993a6c5c5a3250f5dc3a7827e3476
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2025-09-25 15:32:51 +0800

    添加进程生成示例的输出注释

--- main.cpp
+++ main.cpp
@@ -8,3 +8,10 @@ int main(void)
   pid_t y = fork();
   printf("PID: %-5d | x: %-5d | y: %-5d\n", getpid(), x, y);
 }
+
+/**
+ * PID: 30387 | x: 30388 | y: 30389
+ * PID: 30389 | x: 30388 | y: 0
+ * PID: 30388 | x: 0     | y: 30390
+ * PID: 30390 | x: 0     | y: 0
+ */
