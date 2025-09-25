dac66289a3fe629a5132f748b5b312a07ad7663d
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2025-09-25 15:32:11 +0800

    PID

--- main.cpp
+++ main.cpp
@@ -1,37 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unistd.h>
 using namespace std;
-
-int ans;
-
-void dfs(int num, int depth)
-{
-	if (num <= depth)
-	{
-		return;
-	}
-	for (int i = depth; i <= sqrt(num); i++)
-	{
-		if (num % i == 0)
-		{
-			ans++;
-			dfs(num / i, i);
-		}
-	}
-}
 
 int main(void)
 {
-	int n;
-	cin >> n;
-	vector<int> arr(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-	}
-	for (int i = 0; i < arr.size(); i++)
-	{
-		ans = 1;
-		dfs(arr[i], 2);
-		cout << ans << endl;
-	}
+  pid_t x = fork();
+  pid_t y = fork();
+  printf("PID: %-5d | x: %-5d | y: %-5d\n", getpid(), x, y);
 }
