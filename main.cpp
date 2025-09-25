4c8c11513832e5f750c3ea9dd641c043576b64be
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2025-09-25 20:13:16 +0800

    移除进程生成相关代码，简化主函数，保留基本输出

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
-#include <unistd.h>
 using namespace std;
 
 int main(void)
 {
-  pid_t x = fork();
-  pid_t y = fork();
-  printf("PID: %-5d | x: %-5d | y: %-5d\n", getpid(), x, y);
+    cout << "Hello, World!" << endl;
 }
 
-/**
- * PID: 30387 | x: 30388 | y: 30389
- * PID: 30389 | x: 30388 | y: 0
- * PID: 30388 | x: 0     | y: 30390
- * PID: 30390 | x: 0     | y: 0
- */
