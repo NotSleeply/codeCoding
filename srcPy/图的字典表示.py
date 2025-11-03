f0d9958e0978340cc55bbff7bc824d514f1a3af1
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2025-11-04 00:55:24 +0800

    refactor: 优化变量命名和注释，增强代码可读性

--- srcPy/图的字典表示.py
+++ srcPy/图的字典表示.py
@@ -8,24 +8,24 @@ if __name__ == "__main__":
 
     for _ in range(n):
 
-        line = input().strip()
-        dicts = eval(line) # {'A': {'B': 5, 'C': 10, 'D': 15}}
+        strLine = input().strip()
+        dictLine = eval(strLine)  # {'A': {'B': 5, 'C': 10, 'D': 15}}
 
-        # 获取节点
-        frist = dicts.keys()  # dict_keys(['A'])
+        # 获取节点 A
+        frist = dictLine.keys()  # dict_keys(['A'])
         frist_list = list(frist)  # ['A']
         node_header = frist_list[0]  # A
         nodes.add(node_header)
 
-        # A:{}
-        dicts_nodes_by = dicts[node_header] # {'B': 5, 'C': 10, 'D': 15}
+        # 获取被链接的节点 A:{}
+        dicts_nodes_by = dictLine[node_header]  # {'B': 5, 'C': 10, 'D': 15}
 
         for dot, length in dicts_nodes_by.items():
             nodes.add(dot)
 
             # 把两个顶点组成的边变成“无向边”的标准形式
-            iterable = sorted([node_header, dot])  # ['A', 'B']
-            edge = tuple(iterable)    # ('A', 'B')
+            listOrder = sorted([node_header, dot])  # ['A', 'B']
+            edge = tuple(listOrder)    # ('A', 'B')
 
             # 添加边
             if edge not in edges:
