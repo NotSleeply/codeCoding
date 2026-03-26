9da85c8199df7571dc22e2adc52b1d285c115a7a
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2026-03-26 09:59:24 +0800

    feat: 移除不再使用的客户端和服务器代码，更新依赖项以支持新的功能

--- gosrc/main.go
+++ gosrc/main.go
@@ -2,88 +2,43 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"sync"
 
-	"github.com/gorilla/websocket" // go get github.com/gorilla/websocket
+	"github.com/gin-gonic/gin"
 )
 
-// 全局连接管理
-var clients = make(map[*websocket.Conn]bool)
-var clientsMutex sync.Mutex
-var clientNames = make(map[*websocket.Conn]string)
-
-// 定义 WebSocket 升级器：把 HTTP 连接升级成 WebSocket
-var upgrader = websocket.Upgrader{
-	// 允许跨域（测试用）
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+// 中间件1
+func middle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("我在方法前，我是1")
+		c.Next()
+		fmt.Println("我在方法后，我是1")
+	}
 }
 
-// 处理 WebSocket 连接
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	// 1. 把 HTTP 连接升级为 WebSocket 连接
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("升级失败：", err)
-		return
-	}
-	// 新连接加入 clients
-	clientsMutex.Lock()
-	clients[conn] = true
-	clientsMutex.Unlock()
-	fmt.Println("客户端已连接！")
-	defer func() {
-		// 断开时移除
-		clientsMutex.Lock()
-		delete(clients, conn)
-		name := clientNames[conn]
-		for c := range clients {
-			c.WriteMessage(websocket.TextMessage, []byte("系统: "+name+" 离开了聊天室"))
-		}
-		delete(clientNames, conn)
-		clientsMutex.Unlock()
-		conn.Close()
-		fmt.Println(name + "已断开！")
-	}()
-	conn.WriteMessage(websocket.TextMessage, []byte("欢迎连接 WebSocket 服务器！你叫什么名字？"))
-	_, msg, err := conn.ReadMessage()
-	if err != nil {
-		fmt.Println("连接失败：", err)
-		return
-	}
-	name := string(msg)
-	clientNames[conn] = name
-	clientsMutex.Lock()
-	for c := range clients {
-		c.WriteMessage(websocket.TextMessage, []byte("系统: "+name+" 进入了聊天室"))
-	}
-	clientsMutex.Unlock()
-
-	// 循环接收/广播消息
-	for {
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("读取消息失败：", err)
-			break
-		}
-		sender := clientNames[conn]
-		broadcastMsg := sender + ": " + string(msg)
-		fmt.Printf(broadcastMsg)
-
-		// 广播给所有连接
-		clientsMutex.Lock()
-		for c := range clients {
-			c.WriteMessage(msgType, []byte(broadcastMsg))
-		}
-		clientsMutex.Unlock()
+// 中间件2
+func middle2() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("我在方法前，我是2")
+		c.Next()
+		fmt.Println("我在方法后，我是2")
 	}
 }
 
 func main() {
-	http.HandleFunc("/ws", wsHandler)
-	fmt.Println("WebSocket 服务启动：ws://127.0.0.1:8081/ws")
-	http.ListenAndServe(":8081", nil)
-	// 测试： wscat -c ws://127.0.0.1:8081/ws
+	r := gin.Default()
+	r.Use(middle()).Use(middle2())
+	r.GET("/", func(c *gin.Context) {
+		fmt.Println("我是方法内部")
+		c.JSON(200, gin.H{"message": "Hello, World!"})
+	})
+	r.Run(":8080")
 }
+
+/*
+输出结果：
+我在方法前，我是1
+我在方法前，我是2
+我是方法内部
+我在方法后，我是2
+我在方法后，我是1
+*/
