24afc9e85d77701d42f344141546bd0ab86a2283
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2026-04-09 23:44:19 +0800

    feat: 优化 main.cpp 中的代码格式，增加可读性

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
-    int n = pow(666*666+233*233,0.5);
-    cout<<n;
+    int n = pow(666 * 666 + 233 * 233, 0.5);
+    cout << n;
 }
